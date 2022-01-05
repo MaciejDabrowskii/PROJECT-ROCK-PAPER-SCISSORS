d4306e5a8c0364cc58e12d9889844b408516a0e1
Jimmy <dabrowski.mac.1@gmail.com>
2022-01-05 18:16:14 +0100

    I added a round counter, a function that removes unnecessary divs, the results are now a multidimensional table from which a table is drawn with each round, I also added a function that resets the game after the 5th round.

--- skrypt.js
+++ skrypt.js
@@ -1,9 +1,48 @@
 //nasluchuje nacisniecia przycisku/ przypisuje wartosc przycisku do wyboru gracza i uruchamia gre
 let button = document.querySelectorAll('button');
 button.forEach(button => button.addEventListener('click', function (e) {
+    
     playerChoice = button.innerText;
+
+    const body = document.body;  
+//funkcja resetuje gre po 5 rundzie
+    if (licznik === 5) {
+        document.body.removeChild(body.querySelectorAll('h1')[0]);
+        document.body.removeChild(body.querySelector('table'));
+        document.body.removeChild(body.querySelectorAll('div')[0])
+        wyniki = [];
+        licznik = 0;
+    }
+
     gra();
+//funkcja usuwa zbedne divy co runde
+    if (licznik >= 1) {
+        document.body.removeChild(body.querySelectorAll('div')[0]);
+        document.body.removeChild(body.querySelector('table'));
+    };
+
+    licznik += 1;
+
+    console.log(makeTable(wyniki));
+//funkcja rysuje tabele z wynikami
+    function makeTable(wyniki) {
+        let table = document.createElement('table');
+        for (let k = 0; k < wyniki.length; k++) {
+            let row = document.createElement('tr');
+            for (let l = 0; l < wyniki[k].length; l++) {
+                let cell = document.createElement('td');
+                cell.textContent = wyniki[k][l];
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
+        }
+        document.body.appendChild(table);
+        return table;
+        
+    }
+    
 } ));
+let licznik = 0;
 let playerChoice = "";
 
 
@@ -17,30 +56,31 @@ function computerPlay() {
 }
 // pojemnik na wynik komputera
 let computerChoice = "";
-// funkcja wybrania zwyciescy rund
+// funkcja wybrania zwyciezcy rund
 function compare(playerChoice, computerChoice) {
     if (playerChoice === "PAPER" && computerChoice === "ROCK") {
-        return "win";
+        return [`Runda: ${licznik +1}`,"win"];
     } else if(playerChoice === "ROCK" && computerChoice === "SCISSORS") {
-        return "win";
+        return [`Runda: ${licznik +1}`,"win"];
     } else if (playerChoice === "SCISSORS" && computerChoice === "PAPER") {
-        return "win";
+        return [`Runda: ${licznik +1}`,"win"];
     } else if (playerChoice === computerChoice) {
-        return "draw";
+        return [`Runda: ${licznik +1}`,"draw"];
     }else {
-        return "loose";
+        return [`Runda: ${licznik +1}`,"loose"];
     }
 }
 // tablica zbierajaca wyniki rund
 let wyniki = []; 
 // pojemnik na ostateczny wynik z 5 rund
-let wynikOstateczny = null;
+let wynikOstateczny = 0;
 // funkcja przeprowadzajaca gre
 function gra() {
-        
         computerChoice = shape[computerPlay()];
+        const div = document.createElement('div');
+        document.body.appendChild(div);
         const p = document.createElement('p');
-        document.body.appendChild(p);
+        div.appendChild(p);
         p.textContent = "Computer: " + computerChoice + " " + "VS " + "Player: " + playerChoice + " -----------> " + compare(playerChoice, computerChoice);
         console.log("Computer say: " + computerChoice);
         console.log("You say: " + playerChoice);
@@ -50,13 +90,13 @@ function gra() {
     winnerCheck();
 }
 
-// funkcja sprawdzajaca ostatecznego zwyciesce z 5 rund
+// funkcja sprawdzajaca ostatecznego zwyciezce z 5 rund
 function winnerCheck() {
     if (wyniki.length === 5) {
         for (let j = 0; j < wyniki.length; j++) {
-            if (wyniki[j] === "win") {
-                wynikOstateczny = +1;
-            } else if (wyniki[j] === "loose") {
+            if (wyniki[j][1] === "win") {
+                wynikOstateczny += 1;
+            } else if (wyniki[j][1] === "loose") {
                 wynikOstateczny = -1;
             }
         }
@@ -69,6 +109,8 @@ function winnerCheck() {
         } else {
             h1.textContent = "PRZEGRALES!!!!";
         }
-    } else {return;
+    } else { 
+        return;
         }
 }
+
