724fd78acb1f47f480a82a7cb85b87490b82c9d7
Jimmy <dabrowski.mac.1@gmail.com>
2022-01-06 18:24:25 +0100

    final version after cleaning js file

--- skrypt.js
+++ skrypt.js
@@ -1,61 +1,32 @@
-//nasluchuje nacisniecia przycisku/ przypisuje wartosc przycisku do wyboru gracza i uruchamia gre
-let button = document.querySelectorAll('button');
-button.forEach(button => button.addEventListener('click', function (e) {
-    
-    playerChoice = button.innerText;
+////////   ZMIENNE GLOBALNE   ////////
 
-    const body = document.body;  
-//funkcja resetuje gre po 5 rundzie
-    if (licznik === 5) {
-        document.body.removeChild(body.querySelectorAll('h1')[0]);
-        document.body.removeChild(body.querySelector('table'));
-        document.body.removeChild(body.querySelectorAll('div')[0])
-        wyniki = [];
-        licznik = 0;
-    }
-
-    gra();
-//funkcja usuwa zbedne divy co runde
-    if (licznik >= 1) {
-        document.body.removeChild(body.querySelectorAll('div')[0]);
-        document.body.removeChild(body.querySelector('table'));
-    };
-
-    licznik += 1;
-
-    console.log(makeTable(wyniki));
-//funkcja rysuje tabele z wynikami
-    function makeTable(wyniki) {
-        let table = document.createElement('table');
-        for (let k = 0; k < wyniki.length; k++) {
-            let row = document.createElement('tr');
-            for (let l = 0; l < wyniki[k].length; l++) {
-                let cell = document.createElement('td');
-                cell.textContent = wyniki[k][l];
-                row.appendChild(cell);
-            }
-            table.appendChild(row);
-        }
-        document.body.appendChild(table);
-        return table;
-        
-    }
-    
-} ));
 let licznik = 0;
 let playerChoice = "";
+let computerChoice = ""; // pojemnik na wynik komputera
+const shape = ["ROCK", "PAPER", "SCISSORS"]; // tablica z ktorej losuje komputer
+let wyniki = []; // tablica zbierajaca wyniki rund
+let wynikOstateczny = 0; // pojemnik na ostateczny wynik z 5 rund
+let buttons = document.querySelectorAll('button');
+const body = document.body;
 
+////////    FUNKCJE    //////// 
 
-// tablica z ktorej losuje komputer
-const shape = ["ROCK", "PAPER", "SCISSORS"];
+//nasluchuje nacisniecia przycisku/ przypisuje wartosc przycisku do wyboru gracza i uruchamia gre
+buttons.forEach(buttons => buttons.addEventListener('click', function (e) {
+    playerChoice = buttons.innerText;
+      
+    reset();
+    gra();
+    cleanDiv();
+    licznik += 1;
+    makeTable(wyniki);
+} ));
+
 // losowanie komputera
 function computerPlay() {
     let computerSay = Math.floor((Math.random()) * shape.length);
     return computerSay;
-   
 }
-// pojemnik na wynik komputera
-let computerChoice = "";
 // funkcja wybrania zwyciezcy rund
 function compare(playerChoice, computerChoice) {
     if (playerChoice === "PAPER" && computerChoice === "ROCK") {
@@ -70,10 +41,7 @@ function compare(playerChoice, computerChoice) {
         return [`Runda: ${licznik +1}`,"loose"];
     }
 }
-// tablica zbierajaca wyniki rund
-let wyniki = []; 
-// pojemnik na ostateczny wynik z 5 rund
-let wynikOstateczny = 0;
+
 // funkcja przeprowadzajaca gre
 function gra() {
         computerChoice = shape[computerPlay()];
@@ -81,12 +49,11 @@ function gra() {
         document.body.appendChild(div);
         const p = document.createElement('p');
         div.appendChild(p);
-        p.textContent = "Computer: " + computerChoice + " " + "VS " + "Player: " + playerChoice + " -----------> " + compare(playerChoice, computerChoice);
+        p.textContent = `Computer: ${computerChoice}  VS  Player: ${playerChoice} -----------> ${compare(playerChoice, computerChoice)}`;
         console.log("Computer say: " + computerChoice);
         console.log("You say: " + playerChoice);
         wyniki.push(compare(playerChoice, computerChoice));
         console.log(wyniki);
-    //}
     winnerCheck();
 }
 
@@ -103,14 +70,48 @@ function winnerCheck() {
         const h1 = document.createElement('h1');
         document.body.appendChild(h1);
         if (wynikOstateczny > 0) {
-            h1.textContent = "WYGRALES!!!!";
+            h1.textContent = "WYGRAŁEŚ!!!!";
         } else if (wynikOstateczny === 0) {
             h1.textContent = "REMIS!!!!!";
         } else {
-            h1.textContent = "PRZEGRALES!!!!";
+            h1.textContent = "PRZEGRAŁEŚ!!!!";
         }
     } else { 
         return;
         }
 }
 
+//funkcja rysuje tabele z wynikami
+function makeTable(wyniki) {
+    let table = document.createElement('table');
+    for (let k = 0; k < wyniki.length; k++) {
+        let row = document.createElement('tr');
+        for (let l = 0; l < wyniki[k].length; l++) {
+            let cell = document.createElement('td');
+            cell.textContent = wyniki[k][l];
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+    document.body.appendChild(table);
+    return table;
+    
+}
+
+//funkcja resetuje gre po 5 rundzie
+function reset() {
+    if (licznik === 5) {
+        body.removeChild(body.querySelectorAll('h1')[0]);
+        body.removeChild(body.querySelector('table'));
+        body.removeChild(body.querySelectorAll('div')[0])
+        wyniki = [];
+        licznik = 0;
+    };
+}
+//funkcja usuwa zbedne divy co runde
+function cleanDiv() {
+    if (licznik >= 1) {
+        document.body.removeChild(body.querySelectorAll('div')[0]);
+        document.body.removeChild(body.querySelector('table'));
+    };
+}
